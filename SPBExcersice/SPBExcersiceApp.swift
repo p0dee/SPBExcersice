da37523f0d585951d05f010e425e1fe8fc304a6b
p0dee <admin@p0dee.com>
2024-05-09 20:17:03 +0900

    Initial Commit

---
+++ SPBExcersice/SPBExcersiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPBExcersiceApp.swift
+//  SPBExcersice
+//
+//  Created by Takeshi Tanaka on 2024/05/09.
+//
+
+import SwiftUI
+
+@main
+struct SPBExcersiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
