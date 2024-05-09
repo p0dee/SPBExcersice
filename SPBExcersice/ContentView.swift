c61ba9274d52837ced7a7e3f990481c150394541
p0dee <admin@p0dee.com>
2024-05-09 20:38:26 +0900

    Implement layout roughly

--- SPBExcersice/ContentView.swift
+++ SPBExcersice/ContentView.swift
@@ -10,12 +10,45 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Spacer()
+                Button {
+                    //TODO: close action
+                } label: {
+                    Image(systemName: "xmark")
+                }
+            }
+            VerticalFixedSpacer(spacing: 10)
+            Text("Hello\nSpeakBUDDY")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            VerticalFixedSpacer(spacing: 85)
+            Spacer() //GraphView
+                .frame(idealHeight: 325)
+                .fixedSize(horizontal: false, vertical: true)
+            VerticalFixedSpacer(spacing: 30)
+            Text("スピークバディで")
+                .font(.title)
+            Text("レベルアップ")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            VerticalFixedSpacer(spacing: 25)
+            Button("プランに登録する") {
+                //TODO: register action
+            }
         }
-        .padding()
+        .padding(.horizontal, 20)
+    }
+}
+
+struct VerticalFixedSpacer: View {
+    let spacing: CGFloat
+    
+    var body: some View {
+        Spacer()
+            .frame(idealHeight: spacing)
+            .fixedSize(horizontal: false, vertical: true)
     }
 }
 
