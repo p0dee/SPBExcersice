580484d9cb8f3560680dd56d38e8e88d9f037d9d
p0dee <admin@p0dee.com>
2024-05-12 22:20:58 +0900

    Implement animation of bar chart stretching

--- SPBExcersice/GraphView.swift
+++ SPBExcersice/GraphView.swift
@@ -22,22 +22,32 @@ struct GraphView: View {
     var body: some View {
         GeometryReader(content: { geometry in
             HStack(alignment: .bottom) {
-                ForEach(datas) { data in
-                    BarElement(data: data)
-                        .frame(height: data.value * geometry.size.height)
+                ForEach(datas.indices, id: \.self) { index in
+                    BarElement(data: datas[index], expandBarAfter: 1.0 + TimeInterval(index) * 0.15)
+                        .frame(height: datas[index].value * geometry.size.height)
                 }
             }
         })
     }
     
-    struct BarElement: View {
+    private struct BarElement: View {
         let data: Data
+        let expandBarAfter: TimeInterval
+        
+        @State private var isBarStretched: Bool = false
         
         var body: some View {
             VStack {
                 UnevenRoundedRectangle(cornerRadii: .init(topLeading: 3, bottomLeading: 0, bottomTrailing: 0, topTrailing: 3), style: .continuous)
+                    .scaleEffect(x: 1.0, y: isBarStretched ? 1 : 0, anchor: .bottom) //FIXME: scaleによってアニメーションをかけると、アニメーション初期にバーの角丸が潰れてしまう問題がある（角丸サイズやdurationの短さからほぼ気がづくのは不可能）
+                    .animation(.easeOut(duration: 0.5), value: isBarStretched)
                 Text(data.title)
             }
+            .onAppear(perform: {
+                Timer.scheduledTimer(withTimeInterval: expandBarAfter, repeats: false) { _ in
+                    isBarStretched = true
+                }
+            })
         }
     }
 }
