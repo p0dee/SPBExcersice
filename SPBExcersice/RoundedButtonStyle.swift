0adbe7d736824c66710e43ab6d25b66d048b33b7
p0dee <admin@p0dee.com>
2024-05-15 21:58:40 +0900

    Add comment for RoundedButtonStyle

--- SPBExcersice/RoundedButtonStyle.swift
+++ SPBExcersice/RoundedButtonStyle.swift
@@ -7,9 +7,16 @@
 
 import SwiftUI
 
+/// Button style for rounded and bordered
 struct RoundedButtonStyle: ButtonStyle {
+    
+    /// Title text color, default value is `.white`
     private var titleColor: Color = .white
+    
+    /// Button fill color, default value is rgba(59, 167, 255, 1)
     private var fillColor: Color = .init(decimalRed: 59, green: 167, blue: 255)
+    
+    /// Button border color, default value is `.white`
     private var borderColor: Color = .white
     
     func makeBody(configuration: Configuration) -> some View {
@@ -31,18 +38,21 @@ struct RoundedButtonStyle: ButtonStyle {
             .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
     }
     
+    /// Set title text color
     func titleColor(_ color: Color) -> Self {
         var result = self
         result.titleColor = color
         return result
     }
     
+    /// Set button fill color
     func fillColor(_ color: Color) -> Self {
         var result = self
         result.fillColor = color
         return result
     }
     
+    /// Set button border color
     func borderColor(_ color: Color) -> Self {
         var result = self
         result.borderColor = color
