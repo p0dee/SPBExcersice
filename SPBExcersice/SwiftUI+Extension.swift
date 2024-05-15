88a787c9c3378abb5effd5c0cc98f167e7174713
p0dee <admin@p0dee.com>
2024-05-15 19:45:46 +0900

    Add comment for View/Color extensions

--- SPBExcersice/SwiftUI+Extension.swift
+++ SPBExcersice/SwiftUI+Extension.swift
@@ -9,6 +9,11 @@ import SwiftUI
 import UIKit
 
 extension View {
+    
+    /// Draw vertical gradient as foreground of this view
+    /// - Parameters:
+    ///   - start: Start (top) color of gradient
+    ///   - end: End (bottom) color of gradient
     func foregroundVerticalGradient(start: Color, end: Color) -> some View {
         self
             .foregroundStyle(LinearGradient(
@@ -20,10 +25,19 @@ extension View {
 }
 
 extension Color {
+    
+    /// Initializer with decimal RGB values
+    /// - Parameters:
+    ///   - red: Red 0...255
+    ///   - green: Green 0...255
+    ///   - blue: Blue 0...255
     init(decimalRed red: Int, green: Int, blue: Int) {
         self = .init(red: Double(red) / 255, green: Double(green) / 255, blue: Double(blue) / 255)
     }
     
+    /// Brighter color
+    /// - Parameter delta: Value to increase brightness of this color
+    /// - Returns: Brighten color
     func brighter(_ delta: Double) -> Color {
         let uiColor = UIColor(self)
         var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0
